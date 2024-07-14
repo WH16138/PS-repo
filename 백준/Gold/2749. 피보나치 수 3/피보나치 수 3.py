b6db3fb280c13cdb06eec327fec0beff9aacfeab
W_H <90103448+WH16138@users.noreply.github.com>
2024-07-14 20:53:29 +0900

    [Gold II] Title: 피보나치 수 3, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,28 @@
+N = int(input())
+q = 1000000
+
+def multiply(A,B):
+    row = len(A)
+    C = [[0]*row for _ in range(row)]
+
+    for r in range(row):
+        for c in range(row):
+            s = 0
+            for k in range(row):
+                s += (A[r][k] * B[k][c])
+            C[r][c] = s%q
+    
+    return C
+
+def square(A, n):
+    if n == 1:
+        return A
+    
+    temp = square(A, n//2)
+    if n%2:
+        return multiply(multiply(temp, temp), A)
+    else:
+        return multiply(temp, temp)
+    
+fib_matrix = [[1,1],[1,0]]
+print(square(fib_matrix,N)[0][1])
