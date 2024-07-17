47d06659291a6c55eec9d141e89312a9b6bdfadb
W_H <90103448+WH16138@users.noreply.github.com>
2024-07-17 21:02:56 +0900

    [Gold V] Title: 동전 1, Time: 192 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,13 +1,8 @@
 n, k = map(int, input().split())
 coin = sorted(list(set([int(input()) for i in range(n)])))
 
-if k < coin[0]:
-    print(0)
-    quit()
-
-dp = [1]+[0 for i in range(k)]
+dp = [1]+[0]*k
 for i in range(len(coin)):
     for j in range(coin[i], k+1):
-        if (j-coin[i]) >= 0:
-            dp[j] += dp[j-coin[i]]
+        dp[j] += dp[j-coin[i]] 
 print(dp[k])
