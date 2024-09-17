96cc3e8c69919cb43acc75fe55847ddb01196d77
W_H <90103448+WH16138@users.noreply.github.com>
2024-09-17 21:25:07 +0900

    [Gold II] Title: Fibonacci Sequence, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/13075. Fibonacci Sequence/Fibonacci Sequence.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(100000)
+N = int(input())
+p = 10**9
+
+def multiply(A,B):
+    row = len(A)
+    C = [[0]*row for _ in range(row)]
+
+    for r in range(row):
+        for c in range(row):
+            s = 0
+            for k in range(row):
+                s += (A[r][k] * B[k][c])
+            C[r][c] = s%p
+    
+    return C
+
+def square(A, n):
+    if n == 1:
+        return A
+    
+    temp = square(A, n//2)
+    if n%2:
+        return multiply(multiply(temp, temp), A)
+    else:
+        return multiply(temp, temp)
+    
+for t in range(N):
+    fib_matrix = [[1,1],[1,0]]
+    print(square(fib_matrix,int(input()))[0][1])
