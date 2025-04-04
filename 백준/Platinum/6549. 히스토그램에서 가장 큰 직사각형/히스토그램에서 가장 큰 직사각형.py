0b1506720f0aa484b4bb6009beb51d5a4641b1c8
W_H <90103448+WH16138@users.noreply.github.com>
2025-04-04 19:09:55 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 1492 ms, Memory: 47704 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -0,0 +1,34 @@
+def solution(histo:list):
+    if len(histo) == 1:
+        return histo[0]
+    elif len(histo) == 2:
+        return max(min(histo)*2, *histo)
+    else:
+        mid = len(histo) // 2
+        left = solution(histo[:mid])
+        right = solution(histo[mid:])
+        result = max(left, right)
+        
+        l,r = mid,mid
+        minh = min(histo[l],histo[r])
+        while True:
+            if 0 < l:
+                if r < len(histo)-1:
+                    if histo[l-1] > histo[r+1]:
+                        l -= 1
+                    else:
+                        r += 1
+                else:
+                    l -= 1
+            elif r < len(histo)-1:
+                r += 1
+            else:
+                break
+            minh = min(minh, histo[l], histo[r])
+            result = max(result, minh*(r-l+1))
+        
+        return result
+
+while (c:=input()) != "0":
+    histogram = list(map(int,c.split()))
+    print(solution(histogram[1:]))
