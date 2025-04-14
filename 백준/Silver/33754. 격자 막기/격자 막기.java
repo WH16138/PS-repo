84acd09ede3c3ee168e718b8c760dba8820d4558
W_H <90103448+WH16138@users.noreply.github.com>
2025-04-14 16:36:06 +0900

    [Silver IV] Title: 격자 막기, Time: 952 ms, Memory: 185656 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/33754. 격자 막기/격자 막기.java
+++ 백준/Silver/33754. 격자 막기/격자 막기.java
@@ -20,9 +20,11 @@ public class Main {
         {
             if (input.nextInt() == 0)
             {
-                if (a[i]==0 || i>0 && a[i-1]==0 || a[i+1]==0 && i<n-1)
+                if (a[i]==0 || i>0 && a[i-1]==0 || i<n-1 && a[i+1]==0)
                 {
                     min = 0;
+                } else {
+                    min = 1;
                 }
             }
         }
