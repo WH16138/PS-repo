71c002f1331896694ade3c45e1889a2e64f41b46
W_H <90103448+WH16138@users.noreply.github.com>
2025-06-01 00:56:51 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.py
@@ -0,0 +1,11 @@
+s,k,h = map(int,input().split())
+if s+k+h >= 100:
+    print("OK")
+else:
+    m = min(s,k,h)
+    if m == s:
+        print("Soongsil")
+    if m == k:
+        print("Korea")
+    if m == h:
+        print("Hanyang")
