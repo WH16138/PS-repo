8e0d8e5f05dd579e8cd915920d54778e7c22b6b7
W_H <90103448+WH16138@users.noreply.github.com>
2025-06-03 18:55:41 +0900

    [Platinum IV] Title: 단절선, Time: 1124 ms, Memory: 94984 KB -BaekjoonHub

---
+++ 백준/Platinum/11400. 단절선/단절선.py
@@ -0,0 +1,57 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def articulations_and_bridges(N, adj):
+    visi_time = [0] * (N + 1)
+    time_low = [0] * (N + 1)
+    visited = [False] * (N + 1)
+    is_art = [False] * (N + 1)
+    bridges = []
+
+    time = 0
+
+    def dfs(u, par):
+        nonlocal time
+        visited[u] = True
+        time += 1
+        visi_time[u] = time
+        time_low[u] = time
+
+        child_count = 0
+
+        for v in adj[u]:
+            if not visited[v]:
+                child_count += 1
+                dfs(v, u)
+                time_low[u] = min(time_low[u], time_low[v])
+                if par != -1 and time_low[v] >= visi_time[u]:
+                    is_art[u] = True
+                if time_low[v] > visi_time[u]:
+                    bridges.append((min(u,v),max(u,v)))
+            elif v != par:
+                time_low[u] = min(time_low[u], visi_time[v])
+        if par == -1 and child_count >=2:
+                is_art[u] = True
+
+    for u in range(1, N+1):
+        if not visited[u]:
+            dfs(u, -1)
+
+    articulation_points = [u for u in range(1, N+1) if is_art[u]]
+    return articulation_points, sorted(bridges)
+
+
+V,E = map(int,input().split())
+
+adj = [[] for _ in range(V+1)]
+
+for e in range(E):
+    u,v = map(int,input().split())
+    adj[u].append(v)
+    adj[v].append(u)
+
+arts, brs = articulations_and_bridges(V, adj)
+print(len(brs))
+for b in brs:
+    print(*b)
