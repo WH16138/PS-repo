2bbf68d29df2fddf87a49f3222296c1f8adb531a
W_H <90103448+WH16138@users.noreply.github.com>
2025-09-09 14:00:47 +0900

    [Gold IV] Title: 어려운 소인수분해, Time: 4876 ms, Memory: 207096 KB -BaekjoonHub

---
+++ 백준/Gold/16563. 어려운 소인수분해/어려운 소인수분해.py
@@ -0,0 +1,22 @@
+def sieve_of_eratosthenes(n):
+    prime = [True for _ in range(n+1)]
+    p = 2
+    while (p**2 <= n):
+        if (prime[p] == True):
+            for i in range(p**2, n+1, p):
+                prime[i] = False
+        p += 1
+    return [p for p in range(2, n+1) if prime[p]]
+
+primes = sieve_of_eratosthenes(5000000)
+for num in map(int, open(0).read().split()[1:]):
+    factors = []
+    for prime in primes:
+        if prime * prime > num:
+            break
+        while (num % prime == 0):
+            factors.append(prime)
+            num //= prime
+    if num > 1:
+        factors.append(num)
+    print(*factors)
