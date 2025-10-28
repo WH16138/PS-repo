dfcf724d338d7ecbb9d6b62af33e1d149404d096
W_H <90103448+WH16138@users.noreply.github.com>
2025-10-28 16:24:23 +0900

    [Gold V] Title: 평범한 배낭, Time: 4648 ms, Memory: 230376 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,21 +1,14 @@
-N,K = map(int,input().split())
-item = list(tuple(map(int,input().split())) for _ in range(N))
+n,k = map(int,input().split())
 
-dp = {}
+stuffs = [0]+[list(map(int,input().split())) for _ in range(n)]
 
-def search(i,w):
-    if dp.get(f'{i} {w}') != None:
-        return dp[f'{i} {w}']
-    else:
-        dp[f'{i} {w}'] = pack(i,w)
-        return dp[f'{i} {w}']
+DP = [[0]*(k+1) for _ in range(n+1)]
 
-def pack(i,w):
-    if i == -1:
-        return 0
-    elif item[i][0] > w:
-        return search(i-1,w)
-    else:
-        return max(item[i][1]+search(i-1,w-item[i][0]), search(i-1,w))
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        if stuffs[i][0] > j:
+            DP[i][j] = DP[i-1][j]
+        else:
+            DP[i][j] = max(DP[i-1][j], DP[i-1][j-stuffs[i][0]] + stuffs[i][1])
 
-print(pack(N-1,K))
+print(DP[n][k])
