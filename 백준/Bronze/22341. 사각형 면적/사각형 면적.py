c5a62930b2c29bf4238ab1c77fabad4a197a0b65
W_H <90103448+WH16138@users.noreply.github.com>
2025-11-22 00:03:59 +0900

    [Bronze II] Title: 사각형 면적, Time: 308 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22341. 사각형 면적/사각형 면적.py
+++ 백준/Bronze/22341. 사각형 면적/사각형 면적.py
@@ -3,7 +3,7 @@ N ,C = map(int,input().split())
 x,y = N,N
 for i in range(C):
     nx,ny = map(int,input().split())
-    if min(x,nx)*y <= x*min(y,ny):
+    if min(x,nx)*y < x*min(y,ny):
         y = min(y,ny)
     else:
         x = min(x,nx)
