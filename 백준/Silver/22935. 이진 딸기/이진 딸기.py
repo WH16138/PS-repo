83381a1f5719e92abc90a71915a831e9dcbd50af
W_H <90103448+WH16138@users.noreply.github.com>
2026-01-01 16:13:22 +0900

    [Silver V] Title: 이진 딸기, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/22935. 이진 딸기/이진 딸기.py
+++ 백준/Silver/22935. 이진 딸기/이진 딸기.py
@@ -1,4 +1,4 @@
 for i in range(int(input())):
-    n = (int(input())-1)%28+1
-    bit = min(n,30-n)
-    print(bin(bit)[2:].zfill(4).replace("0","V").replace("1","딸기"))
+  n = (int(input())-1)%28+1
+  bit = min(n,30-n)
+  print(bin(bit)[2:].zfill(4).replace("0","V").replace("1","딸기"))
